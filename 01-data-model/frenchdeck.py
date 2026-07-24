661dda72ba4d9d0b638b0aad224cfe0a09bff909
postpone <sipenghh@163.com>
2026-07-25 00:49:17 +0800

    learn 01-data-model

--- 01-data-model/frenchdeck.py
+++ 01-data-model/frenchdeck.py
@@ -1,4 +1,6 @@
 import collections
+from random import choice
+from typing import Iterator
 
 Card = collections.namedtuple('Card', ['rank', 'suit'])
 
@@ -7,11 +9,30 @@ class FrenchDeck:
     suits = 'spades diamonds clubs hearts'.split()
 
     def __init__(self):
-        self._cards = [Card(rank, suit) for suit in self.suits
-                                        for rank in self.ranks]
+        self._cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
     def __len__(self):
         return len(self._cards)
 
     def __getitem__(self, position):
         return self._cards[position]
+
+    def __iter__(self) -> Iterator[Card]:
+        return iter(self._cards)  # Make the deck explicitly iterable
+
+bear_card = Card('7', 'diamonds')
+deck = FrenchDeck()
+
+for n, card in enumerate(deck, 1):
+    print(n, card)
+
+print(choice(deck))
+
+suit_values = dict(spades=3, hearts=2, diamonds=1, clubs=0)
+
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+for card in sorted(deck, key=spades_high):
+    print(card)
